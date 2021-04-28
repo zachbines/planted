fd3ea5bbc25b5ced939a1d516f8edd7875deccf9
Zach Bines <zachbines@gmail.com>
2021-04-27 23:15:42 -0400

    added mousover to gallery

--- script-home.js
+++ script-home.js
@@ -8,3 +8,19 @@ heading.addEventListener('mouseleave', function() {
     this.classList.remove('mouse-shimmer');
 })
   
+// when the user drags their mouse over a photo in the gallery:
+// the photo will increase in size by 25%,
+// the rest of the gallery with become slightly opaque
+// the photo will over lap the others 
+
+const gallery = document.querySelectorAll('.gallery-img-container');
+console.log(gallery);
+
+for (let picture of gallery) {
+    picture.addEventListener('mouseover', function() {
+        this.classList.add('hover-pic');
+    })
+    picture.addEventListener('mouseleave', function() {
+        this.classList.remove('hover-pic');
+    })
+}
